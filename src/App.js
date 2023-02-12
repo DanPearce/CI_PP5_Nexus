77847a784aa477f85ec9b6db7b3e38965192843a
DanPearce <daniel_pearce@outlook.com>
2023-02-12 17:56:01 +0000

    Add NavBar component, all links and basic styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import './App.css';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
-      <h1>
-        Welcome to Nexus.
-      </h1>
+      <NavBar />
     </div>
   );
 }
---
+++ src/components/NavBar.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import styles from '../styles/NavBar.module.css'
+
+const NavBar = () => {
+  return (
+    <Navbar bg="light" expand="lg" fixed="top">
+      <Container>
+        <Navbar.Brand href="#home" className={styles.Logo}>Nexus</Navbar.Brand>
+        <Navbar.Text className="text-start">
+          Discover today.
+        </Navbar.Text>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto text-start">
+            <Nav.Link href="#link"><i class="fa-solid fa-plus"></i> Sign Up </Nav.Link>
+            <Nav.Link href="#link"><i class="fa-solid fa-check"></i> Sign In</Nav.Link>
+            <Nav.Link href="#home"><i class="fa-solid fa-square-h"></i> Home</Nav.Link>
+            <Nav.Link href="#link"><i class="fa-solid fa-magnifying-glass"></i> Discover</Nav.Link>
+            <Nav.Link href="#link"><i class="fa-solid fa-house-user"></i> Profile</Nav.Link>
+            <Nav.Link href="#link"><i class="fa-solid fa-xmark"></i> Sign Out</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+  </Navbar>
+  )
+}
+
+export default NavBar
