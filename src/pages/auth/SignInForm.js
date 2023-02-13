33b4de2b4e096e8bf53b97cd52e97dc361d472e7
DanPearce <daniel_pearce@outlook.com>
2023-02-13 10:49:10 +0000

    Add initial styling and code for SignInForm

--- src/pages/auth/SignInForm.js
+++ src/pages/auth/SignInForm.js
@@ -1,8 +1,32 @@
 import React from 'react'
+import Col from 'react-bootstrap/Col'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import { Link } from 'react-router-dom'
+import styles from '../../styles/AuthForm.module.css'
+import app from '../../styles/App.module.css'
 
-const SignInForm = () => {
+function SignInForm() {
   return (
-    <div>Sign In.</div>
+    <Container className={`col-md-8 ${app.Border} ${styles.Background}`}>
+      <Row className='p-4 justify-content-md-center'>
+        <Col className={`text-center ${styles.Logo}`}>
+          <h1>Nexus</h1>
+        </Col>
+        <Col className='text-center mx-auto'>
+          <Container>
+            <h1 className={styles.Heading}>Sign In</h1>
+          </Container>
+          <Container>
+            Don't have an account? &nbsp;
+            <Link>
+              Sign up!
+            </Link>
+          </Container>
+        </Col>
+      </Row>
+    </Container>
+
   )
 }
 
