baaa25b091193eae97420858267af3c451af760f
DanPearce <daniel_pearce@outlook.com>
2023-02-13 11:36:23 +0000

    Add styling for sign in form

--- src/pages/auth/SignInForm.js
+++ src/pages/auth/SignInForm.js
@@ -2,24 +2,58 @@ import React from 'react'
 import Col from 'react-bootstrap/Col'
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
 import { Link } from 'react-router-dom'
 import styles from '../../styles/AuthForm.module.css'
-import app from '../../styles/App.module.css'
+import appStyles from '../../styles/App.module.css'
+import btnStyles from '../../styles/Button.module.css'
 
 function SignInForm() {
   return (
-    <Container className={`col-md-8 ${app.Border} ${styles.Background}`}>
+    <Container className={`col-md-8 ${appStyles.Border} ${styles.Background}`}>
       <Row className='p-4 justify-content-md-center'>
-        <Col className={`text-center ${styles.Logo}`}>
+        <Col className={`text-center mt-auto mb-auto ${styles.Logo}`}>
           <h1>Nexus</h1>
         </Col>
         <Col className='text-center mx-auto'>
           <Container>
-            <h1 className={styles.Heading}>Sign In</h1>
+            <h1 className={`${styles.Heading} mb-3`}>Sign In</h1>
+            <Form>
+              <Form.Group className="mb-3" controlId="username">
+                <Form.Label className='d-none'>Username</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Username"
+                  name='username'
+                  className={styles.Input}
+                />
+                <Form.Text className="text-muted">
+                  We'll never share your email with anyone else.
+                </Form.Text>
+              </Form.Group>
+
+              <Form.Group className="mb-4" controlId="password">
+                <Form.Label className='d-none'>Password</Form.Label>
+                <Form.Control 
+                  type="password"
+                  placeholder="Password"
+                  name='password'
+                  className={styles.Input}
+                />
+              </Form.Group>
+              <Button 
+                variant="primary"
+                type="submit"
+                className={`mb-3 ${btnStyles.Button}`}
+              >
+                Sign In
+              </Button>
+            </Form>
           </Container>
-          <Container>
+          <Container className={styles.LinkContainer}>
             Don't have an account? &nbsp;
-            <Link>
+            <Link className={styles.Link} to='/signup'>
               Sign up!
             </Link>
           </Container>
