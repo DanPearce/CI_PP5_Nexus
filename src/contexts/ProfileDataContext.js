77f653acaa3cef8f57ef3d748c332ef8b6c43a8f
DanPearce <daniel_pearce@outlook.com>
2023-07-08 09:44:20 +0000

    Add ProfileDataContext, followHelper/unfollowHelper to utils and code to follow/onfollow through the DataContext, add this data globally via index.js

---
+++ src/contexts/ProfileDataContext.js
@@ -0,0 +1,62 @@
+import { createContext, useContext, useState } from "react";
+import { axiosRes } from "../api/axiosDefaults";
+import { followHelper, unfollowHelper } from "../utils/Utils";
+
+const ProfileDataContext = createContext();
+const SetProfileDataContext = createContext();
+
+export const useProfileData = () => useContext(ProfileDataContext);
+export const useSetProfileData = () => useContext(SetProfileDataContext);
+
+export const ProfileDataProvider = ({ children }) => {
+  const [profileData, setProfileData] = useState({
+    pageProfile: { results: [] }
+  });
+
+
+  const handleFollow = async (clickedProfile) => {
+    try {
+      const { data } = await axiosRes.post("/followers/", {
+        followed: clickedProfile.id,
+      });
+
+      setProfileData((prevState) => ({
+        ...prevState,
+        pageProfile: {
+          results: prevState.pageProfile.results.map((profile) =>
+            followHelper(profile, clickedProfile, data.id)
+          )
+        }
+      }));
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const handleUnfollow = async (clickedProfile) => {
+    try {
+      await axiosRes.delete(`/followers/${clickedProfile.following_id}/`);
+
+      setProfileData((prevState) => ({
+        ...prevState,
+        pageProfile: {
+          results: prevState.pageProfile.results.map((profile) =>
+            unfollowHelper(profile, clickedProfile)
+          )
+        }
+      }));
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <ProfileDataContext.Provider value={profileData}>
+      <SetProfileDataContext.Provider
+        value={{ setProfileData, handleFollow, handleUnfollow }}
+      >
+        {children}
+      </SetProfileDataContext.Provider>
+    </ProfileDataContext.Provider>
+  );
+};
--- src/utils/Utils.js
+++ src/utils/Utils.js
@@ -16,3 +16,33 @@ export const fetchMoreData = async (resource, setRescource) => {
     
   }
 }
+
+export const followHelper = (profile, clickedProfile, following_id) => {
+  return profile.id === clickedProfile.id
+    ?
+      {
+        ...profile,
+        followers_count: profile.followers_count + 1,
+        following_id,
+      }
+    : profile.is_owner
+    ?
+      { ...profile, following_count: profile.following_count + 1 }
+    :
+      profile;
+};
+
+export const unfollowHelper = (profile, clickedProfile) => {
+  return profile.id === clickedProfile.id
+    ?
+      {
+        ...profile,
+        followers_count: profile.followers_count - 1,
+        following_id: null,
+      }
+    : profile.is_owner
+    ?
+      { ...profile, following_count: profile.following_count - 1 }
+    : 
+      profile;
+};
