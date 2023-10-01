03535351100002dcac8843380dd01341344d084c
DanPearce <daniel_pearce@outlook.com>
2023-10-01 13:28:09 +0000

    Add UserPasswordForm.js to allow users to change their password added routes and styling for this

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import PostCreateForm from './pages/posts/PostCreateForm';
 import PostPage from './pages/posts/PostPage';
 import PostEditForm from './pages/posts/PostEditForm'
 import ProfileEditForm from './pages/profiles/ProfileEditForm';
+import UserPasswordForm from './pages/profiles/UserPasswordForm';
 import LandingPage from './components/LandingPage';
 import { useCurrentUser } from './contexts/CurrentUserContext'
 import './api/axiosDefaults'
@@ -66,7 +67,7 @@ function App() {
           <Route exact path='posts/:id' element={<PostPage />} />
           <Route exact path='posts/:id/edit' element={<PostEditForm />} />
           <Route exact path='/profiles/:id/edit' element={<ProfileEditForm />}/>
-
+          <Route exact path="/profiles/:id/edit/password" element={<UserPasswordForm />} />
         </Routes>
         )}
       </Container>
