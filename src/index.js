749d6d9cf2a1a25ad4a41d1cd88e46d6f0bcc48a
DanPearce <daniel_pearce@outlook.com>
2023-10-02 08:27:07 +0000

    Remove Strict Mode as ready for deployment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+/* Imports */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
@@ -9,15 +10,13 @@ import { ProfileDataProvider } from './contexts/ProfileDataContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <Router>
-      <CurrentUserProvider>
-        <ProfileDataProvider>
-          <App />
-        </ProfileDataProvider>
-      </CurrentUserProvider>
-    </Router>
-  </React.StrictMode>
+  <Router>
+    <CurrentUserProvider>
+      <ProfileDataProvider>
+        <App />
+      </ProfileDataProvider>
+    </CurrentUserProvider>
+  </Router>
 );
 
 reportWebVitals();
